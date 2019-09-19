0e43e9ebda51d83b60a59eebe5bd6e8efe317f2c
Amandine Carlier <dev-amandinecarlier@mailo.com>
2019-09-19 17:22:09 +0200

    modified

---
+++ script.js
@@ -0,0 +1,4 @@
+const button = document.getElementById("enter");
+const input = document.getElementById("userinput");
+const ul = document.querySelector("ul");
+const li= document.createElement("li");
