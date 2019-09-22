6043e3f9144cebe4ecb77456198462bb593554fd
Amandine Carlier <dev-amandinecarlier@mailo.com>
2019-09-22 11:58:56 +0200

    add new text

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const button = document.getElementById("enter");
 const input = document.getElementById("userinput");
 const ul = document.querySelector("ul");
 const li= document.createElement("li");
+
+function inputlength () {
+       return input.value.length;
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+    }
+
+    
